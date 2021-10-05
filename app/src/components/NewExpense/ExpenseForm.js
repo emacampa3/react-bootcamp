600cf589a5d06ba049ca7de5901f691fba0d9d32
Ema Čampa <emacampa3@gmail.com>
2021-10-05 16:28:57 +0200

    using one state instead of multiple

--- app/src/components/NewExpense/ExpenseForm.js
+++ app/src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,56 @@
+import React, { useState } from 'react';
+
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
-	<form>
-		<div className='new-expense__controls'>
-			<div className='new-expense__control'>
-				<label>Title</label>
-				<input type='text' />
+const ExpenseForm = () => { /* instead of using three different useState we can use one by passing in an object instead of string */
+	const [userInput, setUserInput] = useState({ /* one state approach */
+		enteredTitle: '',
+		enteredAmount: '',
+		enteredDate: ''
+	})
+
+	const titleChangeHandler = (event) => { /* we automatically get the event object value at the point of time the event occurs: event.target.value */
+		setUserInput({
+			...userInput, /* manually copying the other data in the object so it does not get lost using the spread operator */
+			enteredTitle: event.target.value,
+		})
+	};
+
+	const amountChangeHandler = (event) => {
+		setUserInput({
+			...userInput,
+			enteredAmount: event.target.value,
+		})
+	};
+	
+	const dateChangeHandler = (event) => {
+		setUserInput({
+			...userInput,
+			enteredDate: event.target.value,
+		})
+	};
+
+	return (
+		<form>
+			<div className='new-expense__controls'>
+				<div className='new-expense__control'>
+					<label>Title</label>
+					<input type='text' onChange={titleChangeHandler} /> {/* pointer to the function that should execute when the event occurs */}
+				</div>
+				<div className='new-expense__control'>
+					<label>Amount</label>
+					<input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+				</div>
+				<div className='new-expense__control'>
+					<label>Date</label>
+					<input type='date' min='2020-01-01' max='2022-12-31' onChange={dateChangeHandler} />
+				</div>
 			</div>
-			<div className='new-expense__control'>
-				<label>Amount</label>
-				<input type='number' min='0.01' step='0.01' />
+			<div className='new-expense__actions'>
+					<button type='submit'>Add Expense</button>
 			</div>
-			<div className='new-expense__control'>
-				<label>Date</label>
-				<input type='date' min='2020-01-01' max='2022-12-31' />
-			</div>
-		</div>
-		<div className='new-expense__actions'>
-			<button type='submit'>Add Expense</button>
-		</div>
-	</form>;
+		</form>
+	);
 };
 
 export default ExpenseForm;
