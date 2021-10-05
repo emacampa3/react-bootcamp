1e82456a19debd17eb422130605e2e03291868c6
Ema Čampa <emacampa3@gmail.com>
2021-10-05 15:54:24 +0200

    new file

--- app/src/components/Expenses/ExpenseDate.js
+++ app/src/components/Expenses/ExpenseDate.js
@@ -6,10 +6,10 @@ const ExpenseDate = () => {
 	const day = props.date.getFullYear();
 
 	return (
-		<div className="expense-date">
-			<div className="expense-date__month">{month}</div>
-			<div className="expense-date__year">{year}</div>
-			<div className="expense-date__day">{day}</div>
+		<div className='expense-date'>
+			<div className='expense-date__month'>{month}</div>
+			<div className='expense-date__year'>{year}</div>
+			<div className='expense-date__day'>{day}</div>
 		</div>
 	);
 }
