2601bb11e23584e11ff65640695aa6848b181972
Ema Čampa <emacampa3@gmail.com>
2021-10-07 08:18:02 +0200

    comments added for stateful components and adding a cleaner version of ExpenseItem.js to use

--- app/src/components/extra-files/ExpenseFilter.js
+++ app/src/components/extra-files/ExpenseFilter.js
@@ -9,7 +9,9 @@ const ExpensesFilter = () => {
 		<div className='expenses-filter'>
 			<div className='expenses-filter__control'>
 				<label>Filter by year</label>
-        {/* two way binding, passing the data from parent to child: from Expenses.js to ExpenseFilter.js  */}
+        {/* two way binding, passing the data from Expenses.js to ExpenseFilter.js and reverse: creating a controlled/stateful component of ExpenseFilter: 
+        the values as well as changes to the values are not handled at the component itself, but rather at a parent component;
+        typical forstateful components: their state is managed and passed around to different components */}
 				<select value={props.selected} onChange={dropdownChangeHandler}> {/* we want to listen to changes on the select element: pointing to a function */}
 					<option value='2022'>2022</option>
 					<option value='2021'>2021</option>
