50925955a9775a983f0a00337379ae5fc3714b4f
Ema Čampa <emacampa3@gmail.com>
2021-10-18 19:45:49 +0200

    made new components

--- app3/src/components/Users/AddUser.js
+++ app3/src/components/Users/AddUser.js
@@ -1,16 +1,21 @@
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
+
 const AddUser = (props) => {
   const addUserHnandler = (event) => {
     event.preventDefault();
   };
 
 	return (
-		<form onSubmit={addUserHnandler}>
-			<label htmlFor='username'>Username</label>
-			<input id='username' type='text'  />
-			<label htmlFor='age'>Age (Years)</label>
-			<input id='age' type='number' />
-      <button type='submit'>Add User</button>
-		</form>
+		<Card className={classes.input}>
+			<form onSubmit={addUserHnandler}>
+				<label htmlFor='username'>Username</label>
+				<input id='username' type='text' />
+				<label htmlFor='age'>Age (Years)</label>
+				<input id='age' type='number' />
+				<button type='submit'>Add User</button>
+			</form>
+		</Card>
 	);
 };
 
