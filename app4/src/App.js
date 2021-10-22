c55a7e90cdf753e02134a429ea3f22001a77cd0b
Ema Čampa <emacampa3@gmail.com>
2021-10-22 15:37:52 +0200

    adding useEffect() in the app

--- app4/src/App.js
+++ app4/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,22 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []); /* runs only once, when the app starts up */
+
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
+		// We should of course check email and password
+		localStorage.setItem("isLoggedIn", "1");
+		setIsLoggedIn(true);
+	};
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLogged')
     setIsLoggedIn(false);
   };
 
