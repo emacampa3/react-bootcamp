979060c8476d98225567c3aef327125d62fc1236
Ema Čampa <emacampa3@gmail.com>
2021-11-01 20:22:51 +0100

    implementing forward refs

--- app4/src/components/UI/Input/Input.js
+++ app4/src/components/UI/Input/Input.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useRef, useImperativeHandle } from 'react'
 
 import classes from './Input.module.css'
 
-const Input = props => {
+const Input = React.forwardRef((props, ref) => { /* enabling input to take in Ref */
+  const inputRef = useRef()
+
+  const activate = () => {
+    inputRef.current.focus()
+  }
+
+  useImperativeHandle(ref, () => { /* takes two arguments */
+    return {
+      focus: activate /* refers to the function */
+    }
+  })
+
   return (
 		<div
 			className={`${classes.control} ${
@@ -11,6 +23,7 @@ const Input = props => {
 		>
 			<label htmlFor={props.id}>props.label</label>
 			<input
+      ref={inputRef}
 				type={props.type}
 				id={props.id}
 				value={props.value}
@@ -19,6 +32,6 @@ const Input = props => {
 			/>
 		</div>
 	)
-}
+})
 
 export default Input
