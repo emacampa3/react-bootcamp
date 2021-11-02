e9cba7d9262a598974bd35d96f1ea5d16df8f03c
Ema Čampa <emacampa3@gmail.com>
2021-11-02 21:21:39 +0100

    adding functionality and styling to the app

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -38,6 +38,7 @@ const AvailableMeals = () => {
 				<ul>
 					{DUMMY_MEALS.map((meal) => (
 						<MealItem
+							id={meal.id}
 							key={meal.id}
 							name={meal.name}
 							description={meal.description}
