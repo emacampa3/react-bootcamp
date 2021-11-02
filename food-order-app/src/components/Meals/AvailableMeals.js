4985942491918946d76e3e657940a54f3bb85718
Ema Čampa <emacampa3@gmail.com>
2021-11-02 21:18:01 +0100

    adding functionality and styling to the app

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
-import classes from './AvailableMeals.module.css'
+import Card from "../UI/Card"
+import MealItem from "./MealItem/MealItem"
+import classes from "./AvailableMeals.module.css"
 
 const DUMMY_MEALS = [
 	{
@@ -28,11 +30,22 @@ const DUMMY_MEALS = [
 ]
 
 const AvailableMeals = () => {
-  /* transforming the array into array of jsx elements */
+	/* transforming the array into array of jsx elements */
 
-  return (
+	return (
 		<section className={classes.meals}>
-			<ul>{DUMMY_MEALS.map(meal => <li>{meal.name}</li>)}</ul>
+			<Card>
+				<ul>
+					{DUMMY_MEALS.map((meal) => (
+						<MealItem
+							key={meal.id}
+							name={meal.name}
+							description={meal.description}
+							price={meal.price}
+						/>
+					))}
+				</ul>
+			</Card>
 		</section>
 	)
 }
