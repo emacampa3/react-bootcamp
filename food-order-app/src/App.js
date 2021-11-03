b3d3fc664c72bcfd37c7cb9e3e3a3472d413c46d
Ema Čampa <emacampa3@gmail.com>
2021-11-03 12:21:14 +0100

    adding a cart context

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,8 +1,9 @@
-import React, { Fragment, useState } from 'react'
+import React, { useState } from 'react'
 
 import Header from "./components/Layout/Header";
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
@@ -16,13 +17,13 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandlerFunction} />}
       <Header onShowCart={showCartHandlerFunction} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
