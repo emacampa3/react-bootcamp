0bd55a815bb1380ed023f2e2d557f3ce63f9afa4
Ema Čampa <emacampa3@gmail.com>
2021-11-04 20:42:09 +0100

    using Refs and Forward Refs to enable updating the  number of items in the cart

--- food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,18 +1,48 @@
-import Input from '../../UI/Input'
-import classes from './MealItemForm.module.css'
+import { useRef, useState } from "react"
 
-const MealItemForm = props => {
-  return <form className={classes.form}>
-    <Input label='Amount' input={{
-      id: 'amount_' + props.id,
-      type: 'number',
-      min: '1',
-      max: '5',
-      step: '1',
-      defaultValue: '1'
-    }} />
-    <button>+ Add</button>
-  </form>
+import Input from "../../UI/Input"
+import classes from "./MealItemForm.module.css"
+
+const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true)
+	const amountInputRef = useRef()
+
+	const submitHandler = (event) => {
+		event.preventDefault()
+
+		const enteredAmount = amountInputRef.current.value
+		const eneteredAmountNumber = +enteredAmount
+
+		if (
+			enteredAmount.trim().length === 0 ||
+			eneteredAmountNumber < 1 ||
+			eneteredAmountNumber > 5
+		) {
+      setAmountIsValid(false)
+			return
+		}
+
+    props.onAddToCart(eneteredAmountNumber)
+	}
+
+	return (
+		<form className={classes.form} onSubmit={submitHandler}>
+			<Input
+				ref={amountInputRef}
+				label='Amount'
+				input={{
+					id: "amount_" + props.id,
+					type: "number",
+					min: "1",
+					max: "5",
+					step: "1",
+					defaultValue: "1",
+				}}
+			/>
+			<button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
+		</form>
+	)
 }
 
 export default MealItemForm
