7c885e1dfb072e7d58b09cd07c4081a299ebfa24
Ema Čampa <emacampa3@gmail.com>
2021-11-07 19:26:26 +0100

    added the logic for removing the item from cart

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -12,28 +12,51 @@ const cartReducerFunction = (state, action) => {
 		const updatedTotalAmount =
 			state.totalAmount + action.item.price * action.item.amount
 		
-			const existingCartItemIndex = state.items.findIndex(
-				(item) => item.id === action.item.id
-			)
-			const existingCartItem = state.items(existingCartItemIndex)
-			let updatedItems
-			
-			if (existingCartItem) {
-				const updatedItem = {
-					...existingCartItem,
-					amount: existingCartItem.amount + action.item.amount
-				}
-				updatedItems = [...state.items]
-				updatedItems[existingCartItem] = updatedItem
-			} else {
-				updatedItems = state.items.concat(action.item)
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.item.id
+		)
+		const existingCartItem = state.items(existingCartItemIndex)
+		let updatedItems
+		
+		if (existingCartItem) {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount + action.item.amount
 			}
-			
+			updatedItems = [...state.items]
+			updatedItems[existingCartItem] = updatedItem
+		} else {
+			updatedItems = state.items.concat(action.item)
+		}
+
+		
 		return {
 			items: updatedItems,
 			totalAmount: updatedTotalAmount,
 		}
 	}
+	if (action.type === 'REMOVE_ITEM') {
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.id
+		)
+		const existingItem = state.items[existingCartItemIndex]
+		const updatedTotalAmount = state.totalAmount - existingItem.price
+		let updatedItems 
+
+		if (existingItem.amount === 1) {
+			updatedItems = state.items.filter(item => item.id !== action.id)
+		} else {
+			const updatedItem = { ...existingItem, amount: existingItem.amount - 1}
+			updatedItems =[...state.item]
+			updatedItems[existingCartItemIndex] = updatedItem
+		}
+
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount
+		}
+	}
+
 	return defaultCartState
 }
 
