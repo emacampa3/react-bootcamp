215392eb06e985ed1ac1c443b868f99bfd96cf03
Ema Čampa <emacampa3@gmail.com>
2021-11-15 10:52:31 +0100

    adding better UI

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   async function fetchMoviesHandler() {
+    setIsLoading(true)
     const response = await fetch("https://swapi.dev/api/films/")
     const data = await response.json()
 
@@ -19,18 +21,21 @@ function App() {
       }
     })
     setMovies(transformedMovies)
+    setIsLoading(false)
   }
 
   return (
-    <React.Fragment>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
-    </React.Fragment>
-  );
+		<React.Fragment>
+			<section>
+				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
+			</section>
+			<section>
+				{!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+				{!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+				{isLoading && <p>Loading ...</p>}
+			</section>
+		</React.Fragment>
+	)
 }
 
 export default App;
