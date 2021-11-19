d46507c7da683cc905d6d1479e1182d99aeeca25
Ema Čampa <emacampa3@gmail.com>
2021-11-19 10:11:46 +0100

    implemented usseCounter() custom hook in BackwardCounter

--- counter-app/src/hooks/use-counter.js
+++ counter-app/src/hooks/use-counter.js
@@ -1,16 +1,20 @@
 /* CUSTOM HOOK */
 import { useState, useEffect } from "react"
-
-const useCounter = () => { /* function nme has to start with "use-" */
+/* function name has to start with "use-" */
+const useCounter = (forwards = true) => { /* conditional use */
   const [counter, setCounter] = useState(0) /* for every component the custom hook is executed again and every component has its own state */
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			setCounter((prevCounter) => prevCounter + 1)
+      if (forwards) { /* if fowrds is true, add 1 */
+        setCounter((prevCounter) => prevCounter + 1)
+      } else { 
+         setCounter((prevCounter) => prevCounter - 1)
+      }
 		}, 1000)
 
 		return () => clearInterval(interval)
-	}, [])
+	}, [forwards]) /* forwards is a dependancy, as the useEffect() should rerun whenever the forwards changes */
 
   return counter /* counter state will later hold an actual number */
 }
