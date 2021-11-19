3c008968ed6db675347a522f18674df95ea7bfd6
Ema Čampa <emacampa3@gmail.com>
2021-11-19 10:51:19 +0100

    made custom hook

--- task-app/src/hooks/use-http.js
+++ task-app/src/hooks/use-http.js
@@ -1,36 +1,37 @@
 import { useState } from "react";
 
-const useHttp = () => {
+const useHttp = (requestConfig, applyData) => {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
 
-const sendRequest = async () => {
-	setIsLoading(true)
-	setError(null)
-  
-	try {
-		const response = await fetch(
-			"https://react-http-6b4a6.firebaseio.com/tasks.json"
-		)
+  const sendRequest = async () => {
+    setIsLoading(true)
+    setError(null)
 
-		if (!response.ok) {
-			throw new Error("Request failed!")
-		}
+    try {
+      const response = await fetch(
+        requestConfig.url, {
+          method: requestConfig.method,
+          headers: requestConfig.headers,
+          body: JSON.stringify(requestConfig.body) /* body is passed in from outside */
+        }
+      )
+      if (!response.ok) {
+        throw new Error("Request failed!")
+      }
 
-		const data = await response.json()
+      const data = await response.json()
+      applyData(data)	
+    } catch (err) {
+      setError(err.message || "Something went wrong!")
+    }
+    setIsLoading(false)
+  }
 
-		const loadedTasks = []
-
-		for (const taskKey in data) {
-			loadedTasks.push({ id: taskKey, text: data[taskKey].text })
-		}
-
-		setTasks(loadedTasks)
-	} catch (err) {
-		setError(err.message || "Something went wrong!")
-	}
-	setIsLoading(false)
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  }
 }
-}
-
 export default useHttp
