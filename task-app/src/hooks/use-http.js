6079f95d4165b6a5c96998f7cdd9488113a6b608
Ema Čampa <emacampa3@gmail.com>
2021-11-19 11:27:05 +0100

    finished implementing custom hook

--- task-app/src/hooks/use-http.js
+++ task-app/src/hooks/use-http.js
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
 
-  const sendRequest = async () => {
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true)
     setError(null)
 
     try {
       const response = await fetch(
         requestConfig.url, {
-          method: requestConfig.method,
-          headers: requestConfig.headers,
-          body: JSON.stringify(requestConfig.body) /* body is passed in from outside */
+          method: requestConfig.method ? requestConfig.method : 'GET',
+          headers: requestConfig.headers ? requestConfig.headers : {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null, /* body is passed in from outside */
         }
       )
       if (!response.ok) {
@@ -26,7 +26,7 @@ const useHttp = (requestConfig, applyData) => {
       setError(err.message || "Something went wrong!")
     }
     setIsLoading(false)
-  }
+  }, [])
 
   return {
     isLoading,
