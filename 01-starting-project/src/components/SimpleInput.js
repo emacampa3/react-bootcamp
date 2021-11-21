573f480f90f26149ba9b50156af8ef23cf9677fa
Ema Čampa <emacampa3@gmail.com>
2021-11-21 15:17:58 +0100

    styled the project and implemented a simple form submission and client-side validation

--- 01-starting-project/src/components/SimpleInput.js
+++ 01-starting-project/src/components/SimpleInput.js
@@ -1,9 +1,45 @@
+import { useRef, useState } from 'react'
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef()
+  const [enteredName, setEnteredName] = useState('') 
+  const [enteredNameIsValid, setEntereNameIsValid] = useState(false)
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value)
+  }
+
+  const formSubmissionHandler = event => {
+    event.preventDefault()
+
+    setEnteredNameTouched(true)
+
+    if (enteredName.trim() === '') {
+      setEntereNameIsValid(false)
+      return
+    }
+
+    setEntereNameIsValid(true)
+
+    console.log(enteredName)
+
+    const enteredValue = nameInputRef.current.value
+    console.log(enteredValue)
+
+    setEnteredName('')
+  }
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+
+  const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
+        {nameInputIsInvalid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
