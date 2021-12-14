edcaf0d81262cffa30e407d2f762fdb6194b68ba
Ema Čampa <emacampa3@gmail.com>
2021-12-14 21:06:41 +0100

    added loading and error handling state

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -4,15 +4,26 @@ import Card from "../UI/Card"
 import MealItem from "./MealItem/MealItem"
 import classes from "./AvailableMeals.module.css"
 
-
 const AvailableMeals = () => {
 	const [meals, setMeals] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
+	const [httpError, setHttpError] = useState(null)
 
 	/* runs only when component is first loaded */
-	useEffect(()=> {
+	useEffect(() => {
 		const fetchMeals = async () => {
-			const response = await fetch("https://reactmeals-59bb5-default-rtdb.europe-west1.firebasedatabase.app/meals.json")
-			const responseData = await response.json() /* firebase specific: responseData is an object, so it needs to be transformed into an array */
+			const response = await fetch(
+				"https://reactmeals-59bb5-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+			)
+
+			if (!response.ok) {
+				throw new Error(
+					"Something went wrong!"
+				) /* if error occurs, lines thereafter wont execute */
+			}
+
+			const responseData =
+				await response.json() /* firebase specific: responseData is an object, so it needs to be transformed into an array */
 
 			const loadedMeals = []
 
@@ -21,15 +32,36 @@ const AvailableMeals = () => {
 					id: key,
 					name: responseData[key].name,
 					description: responseData[key].description,
-					price: responseData[key].price
+					price: responseData[key].price,
 				})
 			}
 
 			setMeals(loadedMeals)
+			setIsLoading(false)
 		}
-		
-		fetchMeals()
+
+		/* handling an error inside a promise */
+		fetchMeals().catch((error) => {
+			setIsLoading(false)
+			setHttpError(error.message)
+		})
 	}, [])
+
+	if (isLoading) {
+		return (
+			<section className={classes.MealsLoading}>
+				<p>Loading ...</p>
+			</section>
+		)
+	}
+
+	if (httpError) {
+		return (
+			<section className={classes.MealsError}>
+				<p>{httpError}</p>
+			</section>
+		)
+	}
 
 	return (
 		<section className={classes.meals}>
