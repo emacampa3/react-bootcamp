93687760640cb266a6f87e935a3b57d822f8de79
Ema Čampa <emacampa3@gmail.com>
2021-12-23 09:14:12 +0100

    new project using react router added with three routes

--- 09-time-to-practice-starting-code/src/App.js
+++ 09-time-to-practice-starting-code/src/App.js
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div>
+import { Route, Switch } from "react-router-dom"
 
-    </div>
-  );
+import AllQuotes from "./pages/AllQuotes"
+import NewQuote from "./pages/NewQuote"
+import QuoteDetail from "./pages/QuoteDetail"
+
+function App() {
+	return (
+		<Switch>
+			<Route path='/quotes'>
+				<AllQuotes />
+			</Route>
+			<Route path='/quotes/:quoteId'>
+				<QuoteDetail />
+			</Route>
+			<Route path='/new-quote'>
+				<NewQuote />
+			</Route>
+		</Switch>
+	)
 }
 
-export default App;
+export default App
---
+++ 09-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -0,0 +1,5 @@
+const Quotedetail = () => {
+  return <h1>Quote Detail Page</h1>
+}
+
+export default Quotedetail
