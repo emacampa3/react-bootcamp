4667006336c047678374e41d76036af0865a76cf
Ema Čampa <emacampa3@gmail.com>
2022-02-13 13:48:28 +0100

    implemented lazy loading for NewQuote

--- quotes-app/src/App.js
+++ quotes-app/src/App.js
@@ -1,31 +1,43 @@
+import React, { Suspense } from "react"
 import { Route, Switch, Redirect } from "react-router-dom"
 
 import AllQuotes from "./pages/AllQuotes"
-import NewQuote from "./pages/NewQuote"
 import QuoteDetail from "./pages/QuoteDetail"
 import NotFound from "./pages/NotFound"
 import Layout from "./components/layout/Layout"
+import LoadingSpinner from "./components/UI/LoadingSpinner"
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"))
 
 function App() {
 	return (
 		<Layout>
-			<Switch>
-				<Route path='/' exact>
-					<Redirect to='/quotes' />
-				</Route>
-				<Route path='/quotes' exact>
-					<AllQuotes />
-				</Route>
-				<Route path='/quotes/:quoteId'>
-					<QuoteDetail />
-				</Route>
-				<Route path='/new-quote'>
-					<NewQuote />
-				</Route>
-				<Route path='*'>{/* fallback route */}
-					<NotFound />
-				</Route>
-			</Switch>
+			<Suspense
+				fallback={
+					<div className='centered'>
+						<LoadingSpinner />
+					</div>
+				}
+			>
+				<Switch>
+					<Route path='/' exact>
+						<Redirect to='/quotes' />
+					</Route>
+					<Route path='/quotes' exact>
+						<AllQuotes />
+					</Route>
+					<Route path='/quotes/:quoteId'>
+						<QuoteDetail />
+					</Route>
+					<Route path='/new-quote'>
+						<NewQuote />
+					</Route>
+					<Route path='*'>
+						{/* fallback route */}
+						<NotFound />
+					</Route>
+				</Switch>
+			</Suspense>
 		</Layout>
 	)
 }
